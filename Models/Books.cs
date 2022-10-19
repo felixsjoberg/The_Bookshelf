a0c8160debf5bdd11828b109e9a475b44ce94cd6
Felix Sjöberg <felixsjoberg95@gmail.com>
2022-10-19 13:46:45 +0200

    JSON & Model synchronized

--- Models/Books.cs
+++ Models/Books.cs
@@ -11,22 +11,18 @@ namespace gruppArbete.Models
         public string Title { get; set; }
         public string Author { get; set; }
 
-        [JsonProperty("ISBN Nr")]
-        public string ISBNNr { get; set; }
+        public string ISBN { get; set; }
         public string Subject { get; set; }
         public string Language { get; set; }
 
-        [JsonProperty("Release Date")]
-        public object ReleaseDate { get; set; }
+        public int ReleaseDate { get; set; }
         public bool Read { get; set; }
         public int Price { get; set; }
-        public string publisher { get; set; }
         public string Type { get; set; }
 
-        [JsonProperty("Nobel Price Winner")]
         public bool NobelPriceWinner { get; set; }
         public bool Ownership { get; set; }
-        //public string Publisher { get; set; }
+        public string Publisher { get; set; }
     }
 
 
