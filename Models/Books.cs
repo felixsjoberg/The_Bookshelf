bdf3e2afbe8548e77ed5a65df31f792c2bf7734c
Simont93 <113181258+Simomt93@users.noreply.github.com>
2022-11-04 12:49:28 +0100

    Committing the presentaion version

--- Models/Books.cs
+++ Models/Books.cs
@@ -30,6 +30,8 @@ namespace gruppArbete.Models
         public string Image { get; set; }
         public string[] Review { get; set; }
 
+        public string Description { get; set; }
+
     }
 
 
